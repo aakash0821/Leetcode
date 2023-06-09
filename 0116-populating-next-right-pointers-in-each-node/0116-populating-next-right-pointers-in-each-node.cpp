5a68c597030ef783b4573308c060ba399faa91bc
aakash0821 <125751224+aakash0821@users.noreply.github.com>
2023-06-09 23:31:26 +0530

    Time: 19 ms (79.69%), Space: 16.7 MB (95.13%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -18,67 +18,88 @@ public:
 
 class Solution {
 public:
-    vector<vector<Node*>> LevelOrder(Node* root)
-    {
-        Node* head=root;
-        vector<vector<Node*>> ans;
-        if(root==NULL)
-        {
-            return ans;
-        }
+//     vector<vector<Node*>> LevelOrder(Node* root)
+//     {
+//         Node* head=root;
+//         vector<vector<Node*>> ans;
+//         if(root==NULL)
+//         {
+//             return ans;
+//         }
         
-        queue<Node*> q;
-        q.push(head);
-        while(!q.empty())
-        {
-            int size=q.size();
-            vector<Node*> level;
-            for(int i=0;i<size;i++)
-            {
-                Node* fronti=q.front();
-                q.pop();
+//         queue<Node*> q;
+//         q.push(head);
+//         while(!q.empty())
+//         {
+//             int size=q.size();
+//             vector<Node*> level;
+//             for(int i=0;i<size;i++)
+//             {
+//                 Node* fronti=q.front();
+//                 q.pop();
 
-                if(fronti->left!=NULL)
-                {
-                    q.push(fronti->left);
-                }
+//                 if(fronti->left!=NULL)
+//                 {
+//                     q.push(fronti->left);
+//                 }
 
-                if(fronti->right!=NULL)
-                {
-                    q.push(fronti->right);
-                }
+//                 if(fronti->right!=NULL)
+//                 {
+//                     q.push(fronti->right);
+//                 }
                 
-                level.push_back(fronti);
-            }
-            ans.push_back(level);
+//                 level.push_back(fronti);
+//             }
+//             ans.push_back(level);
             
-        }
+//         }
         
-        return ans;
-    }
+//         return ans;
+//     }
     
     
-    Node* connect(Node* root) {
-        vector<vector<Node*>> ans;
-        ans=LevelOrder(root);
-        if(root!=NULL)
-        {
-            Node* head=root;
+//     Node* connect(Node* root) {
+//         vector<vector<Node*>> ans;
+//         ans=LevelOrder(root);
+//         if(root!=NULL)
+//         {
+//             Node* head=root;
 
-            head->next=NULL;
-        }
-        for(int i=1;i<ans.size();i++)
-        {
-            int sizee=ans[i].size();
-            for(int j=0;j<sizee-1;j++)
+//             head->next=NULL;
+//         }
+//         for(int i=1;i<ans.size();i++)
+//         {
+//             int sizee=ans[i].size();
+//             for(int j=0;j<sizee-1;j++)
+//             {
+//                 Node* current=ans[i][j];
+//                 Node* post=ans[i][j+1];
+//                 current->next=post;
+//             }
+//             Node* finals=ans[i][sizee-1];
+//             finals->next=NULL;
+//         }
+//         return root;
+//     }
+    
+        Node* connect(Node* root) {
+            Node* black=root;
+            while(black!=NULL && black->left!=NULL)
             {
-                Node* current=ans[i][j];
-                Node* post=ans[i][j+1];
-                current->next=post;
+                Node* test=black;
+                while(true)
+                {
+                    test->left->next=test->right;
+                    if(test->next==NULL)
+                    {
+                        break;
+                    }
+                    test->right->next=test->next->left;
+                    test=test->next;
+                }
+                black=black->left;
             }
-            Node* finals=ans[i][sizee-1];
-            finals->next=NULL;
-        }
+        
         return root;
     }
 };
