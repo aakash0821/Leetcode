ebc5964e47fc053b24503bb30482c507fc3eb1b2
aakash0821 <125751224+aakash0821@users.noreply.github.com>
2023-07-27 14:56:00 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.85%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int p=num;
+        if(num<=9) return num;
+        else if(num%9==0) return 9;
+        else return (num%9);
         
-        while(p>=10)
-        {
-            string s=to_string(p);
-            int sum=0;
-            for(int i=0;i<s.size();i++)
-            {
-               sum+=s[i]-'0'; 
-            }
-            p=sum;
-        }
-        return p;
+       
         
     }
 };
